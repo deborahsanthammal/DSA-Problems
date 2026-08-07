70e20424bdfbb2061699c818dbffc5b58d65bb78
deborahsanthammal <allandeborah66@gmail.com>
2026-08-07 09:09:12 +0530

    add container with most problem

---
+++ Leetcode_Medium/container_with_most_water.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def maxArea(heights: List[int]):
+    l, r = 0, len(heights) - 1
+    res = 0 
+    while l < r:
+        width = r - 1
+        height = min(heights[l], heights[r])
+        area = width * height
+        res = max(res, area)
+        if heights[l] < heights[r]:
+            l += 1
+        else:
+            r -= 1
+
+    return res
+
+
+if __name__ == "__main__":
+    output = maxArea([2,2,2])
+    print(output)
