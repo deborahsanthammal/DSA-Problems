1da1ba090d9ec98d2649c3b160134a688d8e3a69
deborahsanthammal <allandeborah66@gmail.com>
2026-08-07 09:08:48 +0530

    add three sum

---
+++ Leetcode_Medium/three_sum.py
@@ -0,0 +1,30 @@
+from typing import List
+
+def three_sum(nums: List[int]) -> List[List[int]]:
+    nums.sort()
+    output = []
+    i = 0
+    for i , _ in enumerate(nums):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        j = i + 1
+        k = len(nums) - 1
+        
+        while j < k:
+            sum = nums[i] + nums[j] + nums[k]
+            if sum == 0:
+                output.append([nums[i], nums[j], nums[k]])
+                j += 1
+                while nums[j] == nums[j - 1] and j < k:
+                    j+= 1
+            elif sum > 0:
+                k -= 1
+            else:
+                j+=1
+        
+    return output
+
+
+if __name__ == "__main__":
+    output = three_sum([-3, 3, 4, -3, 1, 2])
+    print(output)
