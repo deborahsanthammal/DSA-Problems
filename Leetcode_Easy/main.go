f638bb2266e2d0284a59602c652ecdc7378c3c4d
deborahsanthammal <allandeborah66@gmail.com>
2026-09-02 08:22:06 +0530

    hashmap(easy): is anagram

--- Leetcode_Easy/main.go
+++ Leetcode_Easy/main.go
@@ -5,7 +5,11 @@ import (
 )
 
 func main() {
-	nums := []int{1,2,3,4}
-	var result bool = ContainsDuplicates(nums)
+	// nums := []int{1,2,3,4}
+	// var result bool = ContainsDuplicates(nums)
+	// fmt.Println(result)
+
+	var s, t string = "racecar", "carrace"
+	var result bool = isAnagram(s, t)
 	fmt.Println(result)
 }
