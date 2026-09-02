96bdcf6ed4b3786f03da363054aefe1649c4371c
deborahsanthammal <allandeborah66@gmail.com>
2026-09-02 08:37:24 +0530

    array_hashmap(easy): two sum

--- Leetcode_Easy/main.go
+++ Leetcode_Easy/main.go
@@ -9,7 +9,12 @@ func main() {
 	// var result bool = ContainsDuplicates(nums)
 	// fmt.Println(result)
 
-	var s, t string = "racecar", "carrace"
-	var result bool = isAnagram(s, t)
+	// var s, t string = "racecar", "carrace"
+	// var result bool = isAnagram(s, t)
+	// fmt.Println(result)
+
+	var target int = 7
+	var nums []int = []int{3,4,5,6}
+	var result []int = twoSum(nums, target)
 	fmt.Println(result)
 }
